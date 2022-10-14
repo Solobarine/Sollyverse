fbd7afbfda859dd1283d813fce898829825b6d93
solomon <solobarine@gmail.com>
2022-10-14 17:12:53 +0100

    Create Popup structure and Close Button

--- public/scripts/dest.js
+++ public/scripts/dest.js
@@ -3,7 +3,6 @@ const allDestinations = document.querySelector('#all-destinations')
 const showVacations = (arg) => {
   arg.forEach(item => {
     const index = arg.indexOf(item)
-    console.log(index)
     allDestinations.innerHTML += `<div class="vacation">
                                     <div class="cover">
                                     <p class="vac-location">${arg[index].place}</p>
@@ -13,8 +12,37 @@ const showVacations = (arg) => {
                                     </div>
                                     <p class="cost">$${arg[index].cost}/week</p>
                                     </div>
-                                  </div>` 
+                                  </div>`
   })
+}
+
+const popupContent = (arg,u, ind) => {
+  u.innerHTML = ''
+  u.innerHTML += `<div class="pop-div">
+                                    <div class="showcase"></div>
+                                    <p class="close">&#10006</p>
+                                    <div class="pop-cover">
+                                    <p class="pop-location">${arg[ind].place}</p>
+                                    <div class="pop-sec">
+                                     <p class="pop-country">${arg[ind].country}</p>
+                                     <button class="pop-like">Like</button>
+                                    </div>
+                                    <p class="desc">${arg[ind].overview}</p>
+                                    <p class="pop-cost">$${arg[ind].cost}/week</p>
+                                    </div>
+                                    </div>`
+  //console.log(ind)
+  u.className = 'popup';
+  console.log(arg)
+  console.log(u)
+  // overlay.className = 'overlay';
+
+}
+
+const removePopup = (u, v) => {
+  //u.innerHTML = `<h2 className="hry">Hey</h2>`
+  //u.classList.remove('popup')
+  //v.classList.remove('overlay')
 }
 
 const showImage = (arg) => {
@@ -22,18 +50,49 @@ const showImage = (arg) => {
   const imgArr = Array.from(image)
   imgArr.forEach(item => {
     const index = imgArr.indexOf(item)
-    console.log(index)
     item.style.backgroundImage = arg[index].img0;
     item.style.backgroundSize = 'cover';
   })
-  console.log(image)
 }
 
 const getDestinations = async() => {
   const data = await fetch('/places');
   const json = await data.json()
-  console.log(json)
   showVacations(json)
   showImage(json)
+  popup(json)
+  //const overlay = document.querySelector('#lay');
 }
+
 getDestinations()
+
+// ---------- POP_UP
+const popup = async(js) => {
+  const image = document.querySelectorAll('.vacation');
+  const cover = document.querySelectorAll('.cover');
+  const imgArr = Array.from(image);
+  const h = document.querySelector('#poppin');
+  console.log(h)
+  imgArr.forEach(item => {
+    const index = imgArr.indexOf(item);
+    item.addEventListener('click', (e) => {
+      console.log(index);
+      popupContent(js, h, index)
+      const close = document.querySelector('.close')
+        close.addEventListener('click', () => {
+        h.innerHTML = '';
+        h.classList.remove('popup');
+        console.log('click')
+      })
+    })
+  })
+}
+
+const closeButton = (i) => {
+  document.querySelector(selectors)
+}
+
+const getClose = () => {
+  const close = document.querySelector('.close')
+  console.log(close)
+}
